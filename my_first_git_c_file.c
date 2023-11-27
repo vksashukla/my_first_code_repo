3746319a087fb226ff78925484acb9be1d41e58f
vksashukla <abhishek_shukla@live.com>
2023-11-27 15:18:42 +0530

    Code updated

--- my_first_git_c_file.c
+++ my_first_git_c_file.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-    printf("Hi There this is my first Git code");
+    char Name[10] ;
+
+    printf("Hi There this is my first Git code \n ");
+    printf("Enter your name \n ");
+
+    scanf("%s", &Name);
+
+    printf("hi %s welcome ", Name );
 
     return 0;
+
 }
